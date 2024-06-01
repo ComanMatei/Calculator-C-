d28cc8bc6e4afd336bdb087df23cb317e3164137
ComanMatei <comanmatei91@gmail.com>
2024-06-01 17:02:07 +0300

    Calculator + History

---
+++ Calculator/Calculator/HistoryForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Calculator
+{
+    partial class HistoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.historyBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // historyBox
+            // 
+            this.historyBox.BackColor = System.Drawing.Color.Black;
+            this.historyBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.historyBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.historyBox.ForeColor = System.Drawing.Color.White;
+            this.historyBox.Location = new System.Drawing.Point(0, 0);
+            this.historyBox.Multiline = true;
+            this.historyBox.Name = "historyBox";
+            this.historyBox.ReadOnly = true;
+            this.historyBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.historyBox.Size = new System.Drawing.Size(493, 520);
+            this.historyBox.TabIndex = 0;
+            this.historyBox.TabStop = false;
+            // 
+            // HistoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 520);
+            this.Controls.Add(this.historyBox);
+            this.Name = "HistoryForm";
+            this.Text = "HistoryForm";
+            this.Load += new System.EventHandler(this.HistoryForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox historyBox;
+    }
+}
---
+++ Calculator/HistoryForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calculator
+{
+    public partial class HistoryForm : Form
+    {
+        List<string> historyCalculations = new List<string>();
+        public HistoryForm(List<string> calculationHistory)
+        {
+            InitializeComponent();
+            historyCalculations = calculationHistory;
+        }
+
+        private void HistoryForm_Load(object sender, EventArgs e)
+        {
+            foreach (string calculation in historyCalculations)
+            {
+                historyBox.Text += calculation + Environment.NewLine;
+            }
+            historyBox.SelectionStart = 0;
+            historyBox.SelectionLength = 0;
+            historyBox.HideSelection = true;
+            historyBox.ReadOnly = true;
+        }
+
+    }
+}
